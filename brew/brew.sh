70ef1c689dba539a46bed06bc8ddca300dbeaf23
Miguel Luis <work@elmassas.pt>
2023-11-02 15:41:34 +0000

    Updated vscode settings, added some brew, firefox, git,macos, ssh, tmux, and zsh configs

---
+++ brew/brew.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env zsh
+
+brew update
+
+brew upgrade
+
+brew install \
+    awscli \
+    bat \
+    coreutils \
+    curl \
+    docker \
+    docker-compose \
+    docker-machine \
+    fd \
+    fzf \
+    git \
+    git-delta \
+    git-lfs \
+    fd \
+    golang \
+    helm \
+    glances \
+    httpie \
+    jq \
+    k9s \
+    kubectx \
+    kubernetes-cli \
+    kustomize \
+    minikube \
+    neovim \
+    nmap \
+    node \
+    podman \
+    python \
+    ripgrep \
+    shellcheck \
+    starship \
+    tmux \
+    tree \
+    vim \
+    wget \
+    zsh
+
+brew cleanup
